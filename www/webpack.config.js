a60517563503159a9877fdbae1fc5cbc99bf1177
Nate Catelli <ncatelli@packetfire.org>
2021-08-07 20:32:19 +0000

    copy stylesheet

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "style.css",
+  },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin(['index.html'])
