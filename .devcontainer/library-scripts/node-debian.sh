14e1fc9d2dc01e8a1f4323716c8f248ca58b83a1
Nate Catelli <ncatelli@packetfire.org>
2021-12-06 22:12:13 -0600

    Auto publish site on main builds (#16)
    * remove package-lock.js
    
    * update devcontainer for wasm-pack
    
    * remove explicity clippy install from test.yml
    
    * Add gh_pages job

--- .devcontainer/library-scripts/node-debian.sh
+++ .devcontainer/library-scripts/node-debian.sh
@@ -5,4 +5,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-nvm install stable --reinstall-packages-from=current 
+nvm install --lts
